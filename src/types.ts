9d42f8d63689ec46813d3ccdbc6aa8d6c8113d56
gvalves <gustavo.alves@igen.org.br>
2025-12-18 11:38:59 -0300

    feat: :sparkles: add new utility types

--- src/types.ts
+++ src/types.ts
@@ -15,6 +15,7 @@ export type Transformer<T, U> = (value: T) => U;
 export type Predicate<T> = Transformer<T, boolean>;
 export type MaybePromise<T> = T | Promise<T>;
 export type MaybeArray<T> = T | T[];
+export type NonEmptyArray<T> = [T, ...T[]];
 
 export type DeepPartial<T> = {
     [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
@@ -30,6 +31,11 @@ export type Mutable<T> = {
 
 export type ValueOf<T> = T[keyof T];
 
+export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type Brand<T, Tag extends string> = T & { readonly __brand: Tag };
 
 export type Result<T, E = Error> = { ok: true; value: T } | { ok: false; error: E };
+
+export type SetOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export type SetRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+export type XOR<T, U> = T extends object ? (U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U) : T | U;
